7611a928f1dd80f2b62ebdf522626af9e4b4644b
Daniel Medina <daniel.m@selia.co>
2024-03-10 06:09:17 -0400

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add more patterns as necessary
+  ],
+  // Other Tailwind configuration...
+};
 
